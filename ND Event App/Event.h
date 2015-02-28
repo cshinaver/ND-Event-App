4596552e61fb135e499e66e4113aa95a6e19e8a3
Mary Connolly <mconnol6@nd.edu>
2015-02-27 23:10:06 -0600

    User and Event classes

---
+++ ND Event App/Event.h
@@ -0,0 +1,13 @@
+//
+//  Event.h
+//  ND Event App
+//
+//  Created by Mary Connolly on 2/27/15.
+//  Copyright (c) 2015 MAC. All rights reserved.
+//
+
+#ifndef ND_Event_App_Event_h
+#define ND_Event_App_Event_h
+
+
+#endif
---
+++ ND Event App/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  ND Event App
+//
+//  Created by Mary Connolly on 2/27/15.
+//  Copyright (c) 2015 MAC. All rights reserved.
+//
+
+#ifndef ND_Event_App_User_h
+#define ND_Event_App_User_h
+
+
+#endif
