2653cb852cdd911465ca67634955993fd1d096e4
Mary Connolly <mconnol6@nd.edu>
2015-02-28 03:42:00 -0600

    EventsViewController completed

---
+++ ND Event App/EventsViewController.h
@@ -0,0 +1,23 @@
+//
+//  EventsViewController.h
+//  ND Event App
+//
+//  Created by Mary Connolly on 2/28/15.
+//  Copyright (c) 2015 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Event.h"
+
+@interface EventsViewController : UIViewController
+
+@property Event *event;
+
+@property (weak, nonatomic) IBOutlet UILabel *host;
+@property (weak, nonatomic) IBOutlet UILabel *eventTitle;
+@property (weak, nonatomic) IBOutlet UILabel *start;
+@property (weak, nonatomic) IBOutlet UILabel *end;
+@property (weak, nonatomic) IBOutlet UILabel *location;
+@property (weak, nonatomic) IBOutlet UILabel *eventDescription;
+
+@end
