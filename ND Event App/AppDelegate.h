8b8582151c02067ac961f3daaee6b91745b7a858
Mary Connolly <mconnol6@nd.edu>
2015-02-27 22:38:40 -0600

    Added xcode project

---
+++ ND Event App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ND Event App
+//
+//  Created by Mary Connolly on 2/27/15.
+//  Copyright (c) 2015 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ND Event AppTests/ND_Event_AppTests.m
@@ -0,0 +1,40 @@
+//
+//  ND_Event_AppTests.m
+//  ND Event AppTests
+//
+//  Created by Mary Connolly on 2/27/15.
+//  Copyright (c) 2015 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ND_Event_AppTests : XCTestCase
+
+@end
+
+@implementation ND_Event_AppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
