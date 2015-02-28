3bbf3419d9ece3dfa41a4fc1b469c997e3cccab2
Mary Connolly <mconnol6@nd.edu>
2015-02-28 00:23:11 -0600

    included User.m

---
+++ ND Event App/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  ND Event App
+//
+//  Created by Mary Connolly on 2/28/15.
+//  Copyright (c) 2015 MAC. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
