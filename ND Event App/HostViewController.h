8e509f386396bf49a4a44689d944b6ae47c55a74
Charles Shinaver <bshinaver@gmail.com>
2015-02-27 22:53:14 -0600

    Removed ViewController. Added view controller files for each view, tab bar, and storyboard

---
+++ ND Event App/HostViewController.h
@@ -0,0 +1,13 @@
+//
+//  HostViewController.h
+//  ND Event App
+//
+//  Created by Charles Shinaver on 2/27/15.
+//  Copyright (c) 2015 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HostViewController : UIViewController
+
+@end
