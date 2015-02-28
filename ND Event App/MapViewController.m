feb63bd45a14b8c4fa5bfa87e892fdb868116477
Charles Shinaver <bshinaver@gmail.com>
2015-02-28 03:46:12 -0600

    Added map stuff

--- ND Event App/MapViewController.m
+++ ND Event App/MapViewController.m
@@ -8,9 +8,12 @@
 
 #import "MapViewController.h"
 #import <MapKit/MapKit.h>
+#import "User.h"
+#import "Event.h"
 
 @interface MapViewController () {
     CLLocationManager *lm;
+    NSMutableArray *events;
 }
 
 @end
@@ -23,7 +26,56 @@
     // Get location permission
     lm = [CLLocationManager new];
     [lm requestWhenInUseAuthorization];
+    
+    // Set delegate
+    self.mapView.delegate = self;
+    
+    self.currentUser = [[User alloc] init];
+    User *user1 = [[User alloc] init];
+    user1.username = @"user1";
+    user1.fullName = @"Joe Moran";
+    self.currentUser.friends = [[NSArray alloc] initWithObjects:user1, nil];
+    Event *event1 = [[Event alloc] init];
+    event1.eventTitle = @"Party in Dillon";
+    event1.eventDescription = @"A fun little get together in Dillon ;)";
+    event1.location = @"Dillon. duh";
+    NSCalendar *calendar = [[NSCalendar alloc] init];
+    NSDateComponents *components = [[NSDateComponents alloc] init];
+    [components setYear: 2015];
+    [components setMonth: 5];
+    [components setDay: 22];
+    [components setHour:20];
+    [components setMinute:0];
+    event1.start = [calendar dateFromComponents:components];
+    [components setHour:23];
+    [components setMinute:0];
+    event1.end = [calendar dateFromComponents:components];
+    user1.events = [NSArray arrayWithObjects: event1, nil];
+    self.currentUser.friends = [NSArray arrayWithObjects: user1, nil];
+    
 }
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2DMake(41.700278, -86.230733), 3200, 3200);
+    [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
+    
+    
+    
+    // Iterate through friends and events
+    for (User *friend in self.currentUser.friends)
+    {
+        for (Event *e in friend.events)
+        {
+            if ([e.invitees containsObject:self.currentUser.fullName])
+            {
+                [events addObject:e];
+            }
+        }
+        
+    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
